afe237b3c5e384247e2782aee3caa7adcd5f5c07
Admir Alicic <admir_alicic@yahoo.com>
2018-08-01 07:18:48 +0200

    Add tests

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,15 +1,22 @@
 const utils = require('./utils');
+const expect = require('expect');
 
 it('should add two numbers', () => {
     let res = utils.add(33, 11);
-    if (res !== 44) {
-        throw new Error(`Expected 44, but got ${res}`);
-    }
+
+    expect(res).toBe(44).toBeA('number');
 });
 
 it('should square a number', () => {
     let res = utils.square(4);
-    if (res != 16) {
-        throw new Error(`Expected 16, but got ${res}`)
-    }
-})
+
+    expect(res).toBe(16).toBeA('number');
+});
+
+it('should verify the firstName and lastName are set', () => {
+    let user = {location: 'Tuzla', age: 44};
+
+    res = utils.setName(user, 'Admir Alicic');
+
+    expect(res).toInclude({firstName: 'Admir', lastName: 'Alicic'});
+});
